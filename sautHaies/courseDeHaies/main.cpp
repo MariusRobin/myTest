b4e781e98ad9064d3d92ebeac31dc99db7c4a925
Marius Robin <mrobin007@info-309-07x.iut.bx1>
2017-09-28 10:06:41 +0200

    haie v2 ok

--- sautHaies/courseDeHaies/main.cpp
+++ sautHaies/courseDeHaies/main.cpp
@@ -2,38 +2,77 @@
 #include <unistd.h>
 #include <stdio.h>
 #include "wait.h"
+#include "vector"
+#include "array"
 
 
 using namespace std;
+vector<pid_t> placement;
+int place=1;
 
 int attendre(){
-    return  (8+rand()%(11-8+1));
+    return  (1+rand()%(11-1+1));
 }
 
-int main() {
+void lancerProcessusFils()
+{
     int i, j;
+    int estTombeOuPas;
+
     /* boucle pour creer les 6 processus fils */
     for (i=1; i < 7; i++) {
         switch (fork()) {
         case -1: fprintf(stderr, "Erreur dans %d\n", getpid());
             perror("fork");
             exit(1);
-        case 0:  /* On est dans un fils */
-
+        case 0: /* On est dans un fils */
             for (j=1;j<=4;j++){
                 srand(time(NULL)-getpid());
-                cout <<"Le courreur " << j << " l'équipe " << getpid() << " est parti" << endl;
-                sleep(attendre());
+                estTombeOuPas = (int)(rand() % 10) + 1;
+                if(estTombeOuPas==1&&j>1){
+                    cout << "\n le coureur de l'equipe "<< getpid() << " est tombe\n" << endl;
+                    exit(EXIT_FAILURE);
+
+                }
+                else
+                {
+                    srand(time(NULL)-getpid());
+                    cout <<"Le courreur " << j << " l'équipe " << getpid() << " est parti" << endl;
+                    sleep(attendre());
+
+                }
 
             }
-            cout<<" - "<<getpid()<<endl;
+            cout<<"\n Le coureur de l'équipe "<<getpid()<<" est arrivé !\n"<<endl;
+
+
             /* Ne pas oublier de sortir sinon on cree fact(6) processus */
-            exit(0);
+            exit(2);
         default:/* On est dans le pere; ne rien faire */ ;
         }
     }
-       wait(NULL);
+}
 
+void affichageArrive()
+{
+    int status;
+    for (int i=1;i<=6;i++)
+    {
+        int leWait = wait(&status);
+        if (WEXITSTATUS(status)==2)
+        {
+            placement.push_back(leWait);
+        }
+    }
+    for (auto elem : placement)
+    {
+        cout << place++ << ". " << elem << endl;
+    }
+}
 
+int main() {
+
+    lancerProcessusFils();
+    affichageArrive();
     return 0;
 }
