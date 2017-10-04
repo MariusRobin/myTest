ff82d7faea3f2c1caee44049a2dcce0e5efd750e
Marius Robin <mrobin007@info-tjaardax.iut.bx1>
2017-10-04 19:09:56 +0200

    sautDeHaies

--- sautHaies/courseDeHaies/main.cpp
+++ sautHaies/courseDeHaies/main.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+ #include <iostream>
 #include <unistd.h>
 #include <stdio.h>
 #include "wait.h"
@@ -9,18 +9,19 @@
 using namespace std;
 vector<pid_t> placement;
 int place=1;
+vector<char*> nomEquipe;
 
 int attendre(){
     return  (1+rand()%(11-1+1));
 }
 
-void lancerProcessusFils()
+void lancerProcessusFils(int argc, char *argv[])
 {
     int i, j;
     int estTombeOuPas;
 
     /* boucle pour creer les 6 processus fils */
-    for (i=1; i < 7; i++) {
+    for (i=1; i < argc; i++) {
         switch (fork()) {
         case -1: fprintf(stderr, "Erreur dans %d\n", getpid());
             perror("fork");
@@ -30,33 +31,34 @@ void lancerProcessusFils()
                 srand(time(NULL)-getpid());
                 estTombeOuPas = (int)(rand() % 10) + 1;
                 if(estTombeOuPas==1&&j>1){
-                    cout << "\n le coureur de l'equipe "<< getpid() << " est tombe\n" << endl;
+                    cout << "\n le coureur de l'equipe "<< argv[i] << " est tombé\n" << endl;
                     exit(EXIT_FAILURE);
 
                 }
                 else
                 {
                     srand(time(NULL)-getpid());
-                    cout <<"Le courreur " << j << " l'équipe " << getpid() << " est parti" << endl;
+                    cout <<"Le courreur " << j << " l'équipe " << argv[i] <<" est parti" << endl;
                     sleep(attendre());
 
                 }
 
             }
-            cout<<"\n Le coureur de l'équipe "<<getpid()<<" est arrivé !\n"<<endl;
+            cout<<"\n Le coureur de l'équipe "<<argv[i]<<" est arrivé !\n"<<endl;
 
 
             /* Ne pas oublier de sortir sinon on cree fact(6) processus */
             exit(2);
-        default:/* On est dans le pere; ne rien faire */ ;
+        default:
+            ;/* On est dans le pere; ne rien faire */
         }
     }
 }
 
-void affichageArrive()
+void affichageArrive(int argc, char *argv[])
 {
     int status;
-    for (int i=1;i<=6;i++)
+    for (int i=1;i<argc;i++)
     {
         int leWait = wait(&status);
         if (WEXITSTATUS(status)==2)
@@ -70,9 +72,10 @@ void affichageArrive()
     }
 }
 
-int main() {
+int main(int argc,char *argv[]) {
 
-    lancerProcessusFils();
-    affichageArrive();
+
+    lancerProcessusFils(argc, argv);
+    affichageArrive(argc, argv);
     return 0;
 }
